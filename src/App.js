7b1fbf79de96ff9b01f08584812a8dc5ee98d258
ari1idont <arindam01012001@gmail.com>
2021-04-03 13:19:15 +0530

    eleventwelvecards

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Eleventwelve from './pages/eleventweleve/Eleventwelve'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Eleventwelve></Eleventwelve>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/eleventweleve/Eleventwelve.js
@@ -0,0 +1,40 @@
+import React,{useState} from 'react'
+import './eleventwelve.css'
+
+
+function Eleventwelve() {
+    return (
+        <div className='eleventwelvecontainer'>
+            <div className='flip-card-eleventwelve'>
+                <div className='flip-card-inner-eleventwelve'>
+                    <div className='flip-card-front-eleventwelve'>
+                        <h1>
+                            11<sup>th</sup>
+                        </h1>
+                    </div>
+                    <div class="flip-card-back-eleventwelve">
+                        <h3>
+                            click to gain all the important resources of PCM class 11<sup>th</sup>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div className='flip-card-eleventwelve'>
+                <div className='flip-card-inner-eleventwelve'>
+                    <div className='flip-card-front-eleventwelve'>
+                        <h1>
+                            12<sup>th</sup>
+                        </h1>
+                    </div>
+                    <div class="flip-card-back-eleventwelve">
+                        <h3>
+                            click to gain all the important resources of PCM class 12<sup>th</sup>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Eleventwelve
